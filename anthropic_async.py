ac2721d72cea1c08f6e85bdd422d0e41c1ac6f13
Ryan Kaskel <dev@ryankaskel.com>
2025-10-01 22:38:12 +0100

    Bump version to 2.3.0

--- anthropic_async.py
+++ anthropic_async.py
@@ -3,7 +3,7 @@ title: Anthropic Manifold Pipe (Async)
 authors: justinh-rahb, christian-taillon, Ryan Kaskel
 author_url: https://github.com/ryankask
 funding_url: https://github.com/open-webui
-version: 2.2.0
+version: 2.3.0
 required_open_webui_version: 0.3.17
 license: MIT
 """
